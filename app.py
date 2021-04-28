d4a5756b8ff87606f1b446288ab9771bc20b52b1
Zac Brandenburg <zac@zacharycraig.com>
2021-04-27 20:11:36 -0400

    Final changes before submission

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+"""
+ Author: Zachary Brandenburg
+"""
+
+from datetime import datetime
+
 from flask import Flask, render_template
 
 app = Flask('sdev300flaskapp',
@@ -6,24 +12,30 @@ app = Flask('sdev300flaskapp',
 
 @app.route('/')
 def index():
+    """Routes to the main page"""
     nav = [
         {'name': 'SlevinLabs', 'url': 'https://www.slevinlabs.com'},
         {'name': 'Google', 'url': 'https://www.google.com'},
         {'name': 'Twitter', 'url': 'https://www.twitter.com'}
     ]
 
-    description = '''SlevinLabs is a leading provider of cutting-edge technologies and services, 
-    offering scalable solutions for companies of all sizes. Founded by a group of friends who started by scribbling 
-    their ideas on a piece of paper, today we offer smart, innovative services to dozens of clients worldwide.
-    We built our solutions by closely listening to our potential clientele and understanding their expectations 
-    with our product. We know how to analyze this information and customize our offering to changing market needs. 
-    Why not join our fast growing customer base? Get in touch today to learn more about the Slevin Labs story.'''
+    description = 'SlevinLabs is a leading provider of cutting-edge technologies and services,' \
+                  'offering scalable solutions for companies of all sizes. Founded by a group of ' \
+                  'friends who started by scribbling their ideas on a piece of paper, today we ' \
+                  'offer smart, innovative services to dozens of clients worldwide. We built our ' \
+                  'solutions by closely listening to our potential clientele and understanding ' \
+                  'their expectations with our product. We know how to analyze this information ' \
+                  'and customize our offering to changing market needs. Why not join our fast ' \
+                  'growing customer base? Get in touch today to learn more about the SlevinLabs ' \
+                  'story.'
 
     items = [
         'Provide software solutions in an ordered timely manner',
         'Consult teams to align goals with Agile methodologies',
         'Set upt you business for success'
     ]
+
+    time = datetime.now().isoformat(' ')
 
     return render_template(
         'home.html',
@@ -32,5 +44,6 @@ def index():
         description=description,
         subtitle='WHAT WE DO',
         subsub='Easy. Fast. Secure.',
-        items=items
+        items=items,
+        time=time
     )
